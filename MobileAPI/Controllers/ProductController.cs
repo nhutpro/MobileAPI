db5ed11da7b4485bd4ee70cd94e5832ac1d7bfaf
nhutpro <tnhut803@gmail.com>
2021-12-14 14:28:32 +0700

    first commit

---
+++ MobileAPI/Controllers/ProductController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace MobileAPI.Controllers
+{
+    public class ProductController : ApiController
+    {
+        [Route("hello")]
+        [HttpGet]
+        public IHttpActionResult HelloApi()
+        {
+            try
+            {
+                Database.Database data = new Database.Database();
+                return Ok(data.ExecuteQuery("select * from PRODUCTS"));
+
+
+
+            }
+            catch
+            {
+                return NotFound();
+            }
+        }
+    }
+}
---
+++ MobileAPI/Database/Database.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace MobileAPI.Database
+{
+    public class Database
+    {
+        public static DataTable ReadTable(string StoredProcedureName, Dictionary<string, object> para = null)
+        {
+            try
+            {
+                DataTable resultTable = new DataTable();
+
+
+                string SQLConnectionString = ConfigurationManager.ConnectionStrings["QLSHOP"].ConnectionString;
+                SqlConnection connection = new SqlConnection(SQLConnectionString);
+
+                connection.Open();
+
+                SqlCommand sqlCmd = connection.CreateCommand();
+                sqlCmd.Connection = connection;
+                sqlCmd.CommandText = StoredProcedureName;
+                sqlCmd.CommandType = CommandType.StoredProcedure;
+                if (para != null)
+                {
+                    foreach (KeyValuePair<string, object> data in para)
+                    {
+                        if (data.Value == null)
+                        {
+                            sqlCmd.Parameters.AddWithValue("@" + data.Key, DBNull.Value);
+                        }
+                        else
+                        {
+                            sqlCmd.Parameters.AddWithValue("@" + data.Key, data.Value);
+                        }
+                    }
+                }
+
+                SqlDataAdapter sqlDa = new SqlDataAdapter();
+                sqlDa.SelectCommand = sqlCmd;
+                sqlDa.Fill(resultTable);
+                connection.Close();
+                return resultTable;
+            }
+            catch
+            {
+                return null;
+            }
+
+        }
+
+        public DataTable ExecuteQuery(string query)
+        {
+            string SQLConnectionString = ConfigurationManager.ConnectionStrings["QLSHOP"].ConnectionString;
+            DataTable data = new DataTable();
+            using (SqlConnection connection = new SqlConnection(SQLConnectionString))
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand(query, connection);
+                SqlDataAdapter adapter = new SqlDataAdapter(command);
+                adapter.Fill(data);
+                connection.Close();
+            }
+            return data;
+        }
+    }
+}
